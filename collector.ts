37b3f6d21c417df3987c8ac0a82bd07e9b693873
sim <sim@terra.money>
2021-09-10 17:13:27 +0900

    Init project

---
+++ collector.ts
@@ -0,0 +1,24 @@
+import { LogFinderResult } from "./types"
+
+export const collector = (result: LogFinderResult[]) => {
+  const returnArray: LogFinderResult[] = []
+  result.forEach((value) => {
+    if (value.transformed) {
+      const action = value.transformed.msgType.split("/")[1]
+
+      if (
+        ![
+          "transfer",
+          "send",
+          "delegate",
+          "undelegate",
+          "begin-redelegate",
+        ].includes(action)
+      ) {
+        returnArray.push(value)
+      }
+    }
+  })
+
+  return returnArray.length > 0 ? returnArray : result
+}
