a0f32485895cbcf2027e1eb61c6b99ff6b0959c6
James <james@terra.money>
2021-09-13 18:23:45 +0900

    refactor logfinder

--- execute.ts
+++ execute.ts
@@ -1,11 +1,25 @@
 import { createReturningLogFinder } from "@terra-money/log-finder"
 import { Event } from "@terra-money/terra.js"
-import { LogFindersRuleSet } from "./types"
+import { LogFindersAmountRuleSet, LogFindersActionRuleSet } from "./types"
 
-export const createLogMatcher = (
-  injectedLogFindersRuleSet: LogFindersRuleSet[] = []
+export const createLogMatcherForActions = (
+  injectedLogFindersRuleSet: LogFindersActionRuleSet[] = []
 ) => {
-  const logFindersRuleSet: LogFindersRuleSet[] = [...injectedLogFindersRuleSet]
+  const logFindersRuleSet = [...injectedLogFindersRuleSet]
+
+  const logFinders = logFindersRuleSet.map(({ rule, transform }) =>
+    createReturningLogFinder(rule, transform)
+  )
+  return (events: Event[]) =>
+    events?.flatMap((event) =>
+      logFinders?.map((logFinderFn) => logFinderFn(event))
+    )
+}
+
+export const createLogMatcherForAmounts = (
+  injectedLogFindersRuleSet: LogFindersAmountRuleSet[] = []
+) => {
+  const logFindersRuleSet = [...injectedLogFindersRuleSet]
 
   const logFinders = logFindersRuleSet.map(({ rule, transform }) =>
     createReturningLogFinder(rule, transform)
