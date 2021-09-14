35641dd0a2383494d093dc5f1fd32ea08095ce1b
James <james@terra.money>
2021-09-14 09:52:47 +0900

    change function name

--- create.ts
+++ create.ts
@@ -5,7 +5,7 @@ import terra from "./terra-core"
 import cw20 from "./cw20"
 import amount from "./amount"
 
-export const actionCreate = (network: string) =>
+export const createActionRuleSet = (network: string) =>
   [cw20(), anchor(network), mirror(network), pylon(network), terra()].flat()
 
-export const amountCreate = () => amount()
+export const createAmountRuleSet = () => amount()
